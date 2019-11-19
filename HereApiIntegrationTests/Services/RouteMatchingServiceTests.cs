0eb6d866c34147426a856796013b1bd66f29ae4c
Rafał Dunaj <Rafal.Dunaj@cshark.com>
2019-11-19 19:19:10 +0100

    tests

---
+++ HereApiIntegrationTests/Services/RouteMatchingServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using HereApiIntegration.Adaptors;
+using HereApiIntegration.Definitions.Options;
+using HereApiIntegrationTests.Stubs;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HereApiIntegration.Services.Tests
+{
+    public class RouteMatchingServiceTests
+    {
+        [Fact()]
+        public void GetTest()
+        {
+            ServicePreparation(out IOptions<HereAPICredentials> hereApiCredentialsOptions, out JsonRouteRepository jsonRepository, out Mock<IHttpClientFactory> httpClientFactoryMoq);
+
+            var service = new RouteMatchingService(jsonRepository, httpClientFactoryMoq.Object, hereApiCredentialsOptions, new JsonRouteToGpxAdaptor());
+            var route = service.Get(1);
+
+            Assert.NotNull(route);
+            Assert.Equal(1, route.Waypoints[0].UnitId);
+        }
+
+        [Fact()]
+        public void GetAllTest()
+        {
+            ServicePreparation(out IOptions<HereAPICredentials> hereApiCredentialsOptions, out JsonRouteRepository jsonRepository, out Mock<IHttpClientFactory> httpClientFactoryMoq);
+
+            var service = new RouteMatchingService(jsonRepository, httpClientFactoryMoq.Object, hereApiCredentialsOptions, new JsonRouteToGpxAdaptor());
+            var routes = service.GetAll();
+
+            Assert.NotEmpty(routes);
+        }
+
+        [Fact()]
+        public async Task MatchRouteTest()
+        {
+            ServicePreparation(out IOptions<HereAPICredentials> hereApiCredentialsOptions, out JsonRouteRepository jsonRepository, out Mock<IHttpClientFactory> httpClientFactoryMoq);
+
+            var service = new RouteMatchingService(jsonRepository, httpClientFactoryMoq.Object, hereApiCredentialsOptions, new JsonRouteToGpxAdaptor());
+            var response = await service.MatchRoute(service.Get(1));
+
+            Assert.NotNull(response);
+            Assert.NotEmpty(response.TracePoints);
+        }
+
+        private void ServicePreparation(out IOptions<HereAPICredentials> hereApiCredentialsOptions, out JsonRouteRepository jsonRepository, out Mock<IHttpClientFactory> httpClientFactoryMoq)
+        {
+            var client = new HttpClient();
+            hereApiCredentialsOptions = Options.Create(new HereAPICredentials() { AppId = "PXiCuHPaKy6hKqKV9Bb6", AppCode = "Mdwq9IojkCEoCmOuXdARcQ" });
+            jsonRepository = new JsonRouteRepository();
+            httpClientFactoryMoq = new Mock<IHttpClientFactory>();
+            httpClientFactoryMoq.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(client);
+        }
+    }
+}
---
+++ HereApiIntegrationTests/Stubs/JsonRouteRepository.cs
@@ -0,0 +1,47 @@
+﻿using HereApiIntegration.Model;
+using HereApiIntegration.Repositories;
+using System.Collections.Generic;
+
+namespace HereApiIntegrationTests.Stubs
+{
+    public class JsonRouteRepository : IJsonRouteRepository
+    {
+        public IEnumerable<JsonRoute> GetAll()
+        {
+            return new List<JsonRoute>() {
+                new JsonRoute()
+                {
+                    Waypoints = new List<JsonWaypoint>()
+                    {
+                        new JsonWaypoint()
+                        {   UnitId=1,
+                            Latitude = 5,
+                            Longitude = 5
+                        },
+                        new JsonWaypoint()
+                        {   UnitId=1,
+                            Latitude = 6,
+                            Longitude = 6
+                        }
+                    }
+                },
+                new JsonRoute()
+                {
+                    Waypoints = new List<JsonWaypoint>()
+                    {
+                        new JsonWaypoint()
+                        {   UnitId=2,
+                            Latitude = 15,
+                            Longitude = 15
+                        },
+                        new JsonWaypoint()
+                        {   UnitId=2,
+                            Latitude = 16,
+                            Longitude = 16
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
